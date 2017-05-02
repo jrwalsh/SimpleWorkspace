41c3bde07b2a2a2fd5b7f8f51680f634459dc790
Jesse Walsh <jrwalsh@iastate.edu>
2017-05-02 10:28:02 -0500

    Added imput files to the repository and moved this mini project to its own folder

--- B73_v3 to v4 mapping/V3 to V4 Mapping.R
+++ B73_v3 to v4 mapping/V3 to V4 Mapping.R
@@ -29,11 +29,21 @@
 # summary(maizegdb.unique$V4_gene %in% gramene.unique$V4_gene)
 
 
-
-# Again...
+# Load packages
 library(data.table)
+library(readxl)
 
-MaizeGDB_v3_v4.genes <- read.delim("~/Dropbox/v3_v4_assoc/MaizeGDB_v3_v4.genes.txt")
+# Import Data
+# MaizeGDB_v3_v4.genes <- read.delim("~/Dropbox/v3_v4_assoc/MaizeGDB_v3_v4.genes.txt")
+MaizeGDB_v3_v4.genes <- read_excel("B73_v3 to v4 mapping/Data/MaizeGDB_v3_v4.genes.xlsx")
+# gramene.new <- read.delim("~/Dropbox/v3_v4_assoc/gramene.new")
+gramene.new <- read.delim("~/Dropbox/v3_v4_assoc/gramene.new")
+# maizegdb.john <- read.delim2("~/Dropbox/v3_v4_assoc/maizegdb.john.txt")
+maizegdb.john <- read.delim2("~/Dropbox/v3_v4_assoc/maizegdb.john.txt")
+# gramene.old <- read.delim("~/Dropbox/v3_v4_assoc/gramene.old.txt")
+gramene.old <- read.delim("~/Dropbox/v3_v4_assoc/gramene.old.txt")
+
+
 MaizeGDB_v3_v4.genes <- MaizeGDB_v3_v4.genes[c(1,2,8,9,20,21)]
 colnames(MaizeGDB_v3_v4.genes) <- c("V3_gene", "V3_chr", "V4_gene", "V4_chr", "notes","rejected")
 MaizeGDB_v3_v4.genes <- MaizeGDB_v3_v4.genes[!(MaizeGDB_v3_v4.genes$V4_gene=="na"),]
@@ -41,7 +51,7 @@ MaizeGDB_v3_v4.genes <- MaizeGDB_v3_v4.genes[!(MaizeGDB_v3_v4.genes$V4_gene=="")
 MaizeGDB_v3_v4.genes <- MaizeGDB_v3_v4.genes[!(MaizeGDB_v3_v4.genes$V4_gene=="not in v4"),]
 MaizeGDB_v3_v4.genes <- MaizeGDB_v3_v4.genes[!(substr(MaizeGDB_v3_v4.genes$V4_gene,1,3)=="zma"),]
 
-gramene.new <- read.delim("~/Dropbox/v3_v4_assoc/gramene.new")
+
   #gramene.new.blank <- gramene.new[gramene.new$type=="1-to-0",]
 gramene.new <- gramene.new[!(gramene.new$type=="1-to-0"),]
   #gramene.new <- gramene.new[,c("V3_gene", "V4_gene")]
